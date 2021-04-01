8a4f5341929401b59112731810f9da074b595827
Zhenghai Xue <xuezh@smail.nju.edu.cn>
2021-04-01 14:24:43 +0800

    upgrade the code for metaworld-v2; add set_task.

--- discor/env.py
+++ discor/env.py
@@ -1,6 +1,8 @@
 import gym
 from gym.envs.registration import register
 from metaworld.envs.mujoco.env_dict import ALL_V1_ENVIRONMENTS
+import metaworld
+import random
 
 gym.logger.set_level(40)
 
@@ -12,18 +14,23 @@ def assert_env(env):
 
 
 METAWORLD_TASKS = (
-    'hammer-v1', 'stick-push-v1', 'push-wall-v1',
-    'stick-pull-v1', 'dial-turn-v1', 'peg-insert-side-v1')
-
-for task in METAWORLD_TASKS:
-    register(
-        id=task,
-        entry_point=ALL_V1_ENVIRONMENTS[task],
-        max_episode_steps=150)
-    assert_env(gym.make(task))
-
+    'hammer-v2', 'stick-push-v2', 'push-wall-v2',
+    'stick-pull-v2', 'dial-turn-v2', 'peg-insert-side-v2')
 
 def make_env(env_id):
-    env = gym.make(env_id)
+    if env_id in METAWORLD_TASKS:
+        ml1 = metaworld.MT1(env_id)
+        env = ml1.train_classes[env_id]()
+        # task = random.choice(ml1.train_tasks)
+        task = ml1.train_tasks[0]
+        env.set_task(task)
+        setattr(env, '_max_episode_steps', 150)
+    else:
+        env = gym.make(env_id)
     setattr(env, 'is_metaworld', env_id in METAWORLD_TASKS)
+    assert_env(env)
     return env
+
+if __name__ == '__main__':
+    env = make_env("stick-pull-v2")
+    print(env.reset())
