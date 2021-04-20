96d91cd46011a4f1b9367b59c9219fe2c675a545
Zhenghai Xue <181220063@smail.nju.edu.cn>
2021-04-20 21:21:28 +0800

    add code to extract info from tfevent and plot

---
+++ experiments/res_plot/extract_reward.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import tensorflow.compat.v1 as tf
+import numpy as np
+sns.set_style()
+
+EXP = "Ant-v2"
+AlGOS = ["discor_full", "lfiw_sac_full", "sac_full"]
+root_path = os.path.join("../../logs/"+EXP)
+
+def get_rew_from_path(path):
+    event_file = os.listdir(os.path.join(root_path, path, "summary"))[0]
+    event_path = os.path.join(root_path, path, "summary", event_file)
+    rewards = []
+    for event in tf.train.summary_iterator(event_path):
+        for value in event.summary.value:
+            if value.tag == "reward/test":
+                rewards.append(value.simple_value)
+    return rewards
+
+algo_paths = {}
+for algo in AlGOS:
+    algo_paths.update({algo:[]})
+
+for dir in os.listdir(root_path):
+    if "full" in dir:
+        for algo in AlGOS:
+            if algo in dir:
+                algo_paths[algo].append(dir)
+
+for algo in AlGOS:
+    paths = algo_paths[algo]
+    file = os.path.join(root_path, "%s-all.txt"%algo)
+    with open(file, 'w') as f:
+        for path in paths:
+            single_seed_reward = get_rew_from_path(path)
+            for reward in single_seed_reward:
+                f.write("%.1f "%reward)
+            f.write("\n")
--- experiments/res_plot/plot.py
+++ experiments/res_plot/plot.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_style()
+import os
+import numpy as np
+sns.set()
 
 EXP = "Ant-v2"
+AlGOS = ["lfiw_sac_full", "sac_full"]
+colors = {
+    "lfiw_sac_full": 'red',
+    'sac_full': 'blue'
+}
+labels = {
+    'lfiw_sac_full': "lfiw_sac",
+    'sac_full': 'sac'
+}
+MAX_STEP=4e6
+# AlGOS = ["discor_full", "lfiw_sac_full", "sac_full"]
+root_path = os.path.join("../../logs/"+EXP)
+
+for algo in AlGOS:
+    file = os.path.join(root_path, "%s-all.txt"%algo)
+    with open(file, 'r') as f:
+        content = f.readlines()
+        all_rewards = []
+        for line in content:
+            all_rewards.append([eval(i) for i in line.split(" ")[:-1]])
+        all_rewards = np.array(all_rewards)
+    rew_mean = np.mean(all_rewards, axis=0)
+    rew_std = np.std(all_rewards, axis=0)
+    x = np.arange(0, MAX_STEP, 5e3)[:len(rew_mean)]
+    plt.plot(x, rew_mean, color=colors[algo], label=labels[algo])
+    plt.fill_between(x, rew_mean - rew_std, rew_mean + rew_std, lw = 3, color = colors[algo], alpha = 0.1)
+plt.legend()
+plt.title(EXP)
+plt.xlabel("Timestep")
+plt.ylabel("Reward")
+plt.savefig("reward-%s.png"%EXP)
